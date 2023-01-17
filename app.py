40f8d7d48e8eb5f9f4618dd61cccaf4f07ee4896
Danilo Brandao <brandaodanilog@gmail.com>
2023-01-17 17:35:40 +0000

    search timespan specification. not working yet.

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ import twitter_auth
 import db
 import tweepy
 from tqdm import tqdm
+from datetime import datetime, timedelta
 
-def main(search_term):
+def main(search_term, days_in_time):
+    # now = datetime.now()
+    # x_days_ago = now - timedelta(days=days_in_time)
+    
+    # # Convert datetime to string
+    # since_date = x_days_ago.strftime("%Y-%m-%d")
+    # until_date = now.strftime("%Y-%m-%d")
+    
     # Authenticate with Twitter API
     api = twitter_auth.authenticate()
     
@@ -11,7 +19,10 @@ def main(search_term):
     conn = db.create_connection()
     
     # Retrieve tweets
-    for page in tqdm(tweepy.Cursor(api.search_tweets, q=search_term, count=100).pages(100), desc='Retrieving tweets:'):
+    for page in tqdm(tweepy.Cursor(api.search_tweets, 
+                                   q=search_term,
+                                   count=100).pages(100), 
+                    desc='Retrieving tweets:'):
         for tweet in page:
             # Insert tweet into database
             db.insert_tweet(conn, tweet)
@@ -21,4 +32,5 @@ def main(search_term):
     
     
 if __name__ == '__main__':
-    main(search_term = "Lula")  # insert search term here
+    main(search_term = "Bolsonaro",  # insert search term here
+         days_in_time=30)  # how many days the search goes back in time
